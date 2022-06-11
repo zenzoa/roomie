f24f903b33981db6b53dd21a256ae0ae5f5faf91
SG <sarah.gould@gmail.com>
2022-06-11 10:43:05 -0700

    add application icon

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 /*
 - fix snap-to-edge when it's just edge-to-edge
 - splash screen for when there's no metaroom loaded
-- application icon
 */
 
 const { api } = require('./script/api')
@@ -9,10 +8,6 @@ const { buildMenu } = require('./script/menu')
 
 nw.Window.open('index.html', {}, mainWindow => {
 	mainWindow.menu = buildMenu()
-
-	mainWindow.width = 800
-	mainWindow.height = 600
-	mainWindow.title = "Roomie"
 
 	mainWindow.window.api = api
 
